47a0724f21f8d11cf56f2b01d139676a270f82f6
7841frank <franklinewax@gmail.com>
2022-02-15 11:23:51 +0300

    home cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,67 @@
 import React from 'react';
+import {Card, Col, Row} from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+import './home.css';
 
 const Home = () => {
     return (
-        <div>
-            Home Page
+        <div className="home-cards">
+            <Row>
+                <Col md={6}>
+                    <Card >
+                        <Card.Header>Acive Loads</Card.Header>
+                        <Card.Body className="text-center">
+                            <Card.Title>Create Your First Load</Card.Title>
+                            <Card.Text>
+                            Create a load and calculate the load charges for a customer by adding the shipper, consignee, and freight details.
+                            </Card.Text>
+                            <Link  to="/" variant="primary">Create Load</Link>
+                        </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={6}>
+                    <Card >
+                        <Card.Header>Assigned Dispatch</Card.Header>
+                        <Card.Body className="text-center">
+                            <Card.Title>Create Your First Dispatch!</Card.Title>
+                            <Card.Text>
+                            Create a Quick Dispatch to assign, enter trip sheets, and add expenses for the dispatch.
+                            </Card.Text>
+                            <Link  to="/" variant="primary">Quick Dispatch</Link>
+                        </Card.Body>
+                       
+                        </Card>
+                    </Col>
+
+                </Row>
+                <Row className="space-btn">
+                <Col md={6}>
+                    <Card >
+                        <Card.Header>Open Invoices</Card.Header>
+                        <Card.Body className="text-center">
+                            <Card.Title>Generate Your First Invoice!</Card.Title>
+                            <Card.Text>
+                            Create invoices to your clients and record payments you receive from them.
+                            </Card.Text>
+                            <Link  to="/" variant="primary">Create Invoice</Link>
+                        </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={6}>
+                    <Card >
+                        <Card.Header>Address Book</Card.Header>
+                        <Card.Body className="text-center">
+                            <Card.Title>Create Your First Contact!</Card.Title>
+                            <Card.Text>
+                            Create a contact by adding Customer, Driver, Carrier and Broker.
+                            </Card.Text>
+                            <Link  to="/" variant="primary">Add Contact</Link>
+                        </Card.Body>
+                       
+                        </Card>
+                    </Col>
+
+                </Row>
         </div>
     )
 }
