1b32ea42f4689ccee43c5f0cbbb2818da50dc893
kimanimargaret46@gmail.com <kimanimargaret46@gmail.com>
2022-02-16 17:04:55 +0300

    loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import ReactDOM from 'react-dom';
 import './loader.css';
-import { useState } from "react";
+// import { useState } from "react";
 
 
 export default function loader() {
@@ -10,47 +10,10 @@ export default function loader() {
           Loader
           <div class="row">
            <div class="column">
-           <form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
-</form>
-
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
+             
            </div>
              <div class="column"></div>
-              </div>
+             </div>
         </div>
     )
 }
