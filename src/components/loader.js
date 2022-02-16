b0508ef16ea6e5414df95c218efcded24b77ca4d
kimanimargaret46@gmail.com <kimanimargaret46@gmail.com>
2022-02-16 14:37:22 +0300

    load number

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import './loader.css';
+// import { useState } from "react";
+
+
 export default function loader() {
     return (
         <div>
@@ -20,3 +23,20 @@ function MyForm() {
 }
 
 ReactDOM.render(<MyForm />, document.getElementById('root'));
+// function MyForm() {
+//   const [number, setNumber] = useState("");
+//   return (
+//     <form>
+//       <h2>Create Load</h2>
+//       <label>Load number:
+//         <input 
+//         type="text"
+//         value={"number"}
+//         onChange={(e) => setNumber(e.target.value)}
+//          />
+//       </label>
+//     </form>
+//   )
+// }
+
+// ReactDOM.render(<MyForm />, document.getElementById('root'));
